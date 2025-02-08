56dcc7d8bde0440dd9aa0ee8136d3355f11c416f
Joshua Nwajei <nwajeijoshua55@gmail.com>
2025-02-08 22:47:36 +0100

    run

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function footer() {
     <div  className='footer-container'  > <center><div className="footer-content">Let's  <span  className='colab'>Collaborate</span> to achieve  Great Result</div>
     
     <div className="font-awsomme-contain">
-    <div id='hov'  class="linkedin"><a href="https:/www.linkedin.com/in/joshua-nwajei-479262344?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=andriod_app">  <i class="fab fa-linkedin" ></i> </a></div>
+    <div id='hov'  class="linkedin"><a href="https://www.linkedin.com/in/joshua-nwajei-479262344?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=andriod_app">  <i class="fab fa-linkedin" ></i> </a></div>
     <div id='hov'  class="git-hub"><a href="https://github.com/Nwajei06"> <i class="fab fa-github"></i></a></div>
     <div id='hov' class="gmail"><a href="mailto:nwajeijoshua55gmail.com">  <i class="fab fa-google "></i></a></div>
     <div id='hov' class="twitter"> <a href="https://x.com/Joshua590015513?t=dSHWvv4I1BTIUIEEW1jVDQ&S=09"> <i class="fab fa-twitter" ></i></a>   </div>
