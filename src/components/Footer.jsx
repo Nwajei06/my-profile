9136fca4412524e00e5ba7d4a0713bb6e118ecfb
Joshua Nwajei <nwajeijoshua55@gmail.com>
2025-04-24 22:51:06 +0100

    commt

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function footer() {
     <div id='hov'  class="linkedin"><a href="https://www.linkedin.com/in/joshua-nwajei-479262344?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=andriod_app">  <i class="fab fa-linkedin" ></i> </a></div>
     <div id='hov'  class="git-hub"><a href="https://github.com/Nwajei06"> <i class="fab fa-github"></i></a></div>
     <div id='hov' class="gmail"><a href="mailto:nwajeijoshua55gmail.com">  <i class="fab fa-google "></i></a></div>
-    <div id='hov' class="twitter"> <a href="https://x.com/Joshua590015513?t=dSHWvv4I1BTIUIEEW1jVDQ&S=09"> <i class="fab fa-twitter" ></i></a>   </div>
+    <div id='hov' class="twitter"> <a href="https://x.com/Apispecialist1?t=xFufD4KHH22gWRxn7sI3Cw&s=09"> <i class="fab fa-twitter" ></i></a>   </div>
 
     </div>
     </center>
